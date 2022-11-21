ccb029571afa40cc09a18d5468d9c4c8aa909d3a
Kingnick253 <nicholaswebb2016@gmail.com>
2022-11-20 19:53:15 -0800

    added webpackmanifest to create manifestjson file

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -23,8 +23,27 @@ module.exports = () => {
         title: 'Webpack Plugin',
       }),
      
-      
+
+      new WebpackPwaManifest({
+        fingerprints: false,
+        inject: true,
+        name: 'Contact Cards',
+        short_name: 'Contact',
+        description: 'Never forget your contacts!',
+        background_color: '#225ca3',
+        theme_color: '#225ca3',
+        start_url: './',
+        publicPath: './',
+        icons: [
+          {
+            src: path.resolve('src/images/logo.png'),
+            sizes: [96, 128, 192, 256, 384, 512],
+            destination: path.join('assets', 'icons'),
+          },
+        ],
+      }),
     ],
+
 
     module: {
       rules: [
@@ -38,14 +57,15 @@ module.exports = () => {
         },
         {
           test: /\.m?js$/,
-          exclude: /(node_modules|bower_components)/,
+          exclude: /node_modules/,
           use: {
             loader: 'babel-loader',
             options: {
-              presets: ['@babel/preset-env']
-            }
-          }
-        }
+              presets: ['@babel/preset-env'],
+              plugins: ['@babel/plugin-proposal-object-rest-spread', '@babel/transform-runtime'],
+            },
+          },
+        },
       ],
     },
   };
