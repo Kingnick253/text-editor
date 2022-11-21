741dddb51559c75e6d070a5ab0a392a61b729311
Kingnick253 <nicholaswebb2016@gmail.com>
2022-11-20 19:54:47 -0800

    added injectManifest plugin for my custom service worker

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -22,7 +22,11 @@ module.exports = () => {
         template: './index.html',
         title: 'Webpack Plugin',
       }),
-     
+      
+      new InjectManifest({
+        swSrc: './src-sw.js',
+        swDest: 'src-sw.js',
+      }),
 
       new WebpackPwaManifest({
         fingerprints: false,
